34bfd5ed527b92bc02e44c5d8a6bf629920d3a51
EliotOrando <eorando@gmail.com>
2023-03-14 14:05:34 -0700

    wrapped linkedin script inside of dom content loaded

--- linkedin-script.js
+++ linkedin-script.js
@@ -1,4 +1,7 @@
-const main = document.getElementById('main');
-console.log('the main element is: ', main);
-main.remove();
-console.log('we are inside of the linkedin scrip');
+addEventListener('DOMContentLoaded', (event) => {
+  const main = document.getElementById('main');
+  console.log('the main element is: ', main);
+  main.remove();
+  console.log('we are inside of the linkedin scrip');
+
+})
