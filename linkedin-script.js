3d7ca2581540477dd3d3740765299c0369a90279
EliotOrando <eorando@gmail.com>
2023-03-14 14:13:17 -0700

    trying to simply console log in the linkedin script

--- linkedin-script.js
+++ linkedin-script.js
@@ -1,3 +1,4 @@
-
-  console.log('we are inside of the linkedin scrip');
+const voyagerFeed = document.getElementById('voyager-feed');
+console.log('this is the voyager feed', voagerFeed);
+console.log('we are inside of the linkedin scrip');
 
