e7ce5a6c75dc16cb8f89934cd500c8214e67705f
EliotOrando <eorando@gmail.com>
2023-03-14 14:07:26 -0700

    trying to simply console log in the linkedin script

--- linkedin-script.js
+++ linkedin-script.js
@@ -1,7 +1,3 @@
-addEventListener('DOMContentLoaded', (event) => {
-  const main = document.getElementById('main');
-  console.log('the main element is: ', main);
-  main.remove();
+
   console.log('we are inside of the linkedin scrip');
 
-})
